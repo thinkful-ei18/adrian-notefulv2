1f8d95db9bf6e8c2aa591010e23d063d55bb92bc
Adrian Ross <adriantoddross@gmail.com>
2018-02-07 15:47:36 -0500

    revise folders.router.js

--- routes/folders.router.js
+++ routes/folders.router.js
@@ -5,49 +5,13 @@ const router = express.Router();
 
 const knex = require('../knex');
 
-router.get('/notes', (req, res, next) => {
-  const searchTerm = req.query.searchTerm ? req.query.searchTerm.toLowerCase() : null;
-
-  knex.select('notes.id', 'title', 'content', 'folder_id', 'folders.name as folder_name')
-    .from('notes')
-    .leftJoin('folders', 'notes.folder_id', 'folders.id')
-    // .where(function () {
-    //   if (searchTerm) {
-    //     this.where('title', 'like', `%${searchTerm}%`);
-    //   }
-    // })
-    // .orWhere(function () {
-    //   if (searchTerm) {
-    //     this.orWhere('content', 'like', `%${searchTerm}%`);
-    //   }
-    // })
-    .where(function () {
-      if (req.query.folderId) {
-        this.where('folder_id', req.query.folderId);
-      }
-    })
-    .orderBy('notes.id')
+router.get('/folders', (req, res, next) => {
+  knex.select('id', 'name')
+    .from('folders')
     .then(results => {
       res.json(results);
     })
-    .catch(err => {
-      console.error(err);
-    });
-});
-
-router.get('/notes/:id', (req, res, next) => {
-  const noteId = req.params.id;
-
-  knex.first('notes.id', 'notes.title', 'notes.content', 'notes.created', 'folder_id', 'folders.name as folder_name').from('notes')
-    .where('id', noteId)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next();
-      }
-    })
-    .catch(err => next(err));
+    .catch(next);
 });
 
 module.exports = router;
