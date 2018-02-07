c789596713e6f99ade3d03ec88be80558bc36666
Adrian Ross <adriantoddross@gmail.com>
2018-02-07 14:57:00 -0500

    add get and get by id method to folders.router

--- routes/folders.router.js
+++ routes/folders.router.js
@@ -5,3 +5,39 @@ const router = express.Router();
 
 const knex = require('../knex');
 
+router.get('/notes', (req, res, next) => {
+  const searchTerm = req.query.searchTerm ? req.query.searchTerm.toLowerCase() : null;
+
+  knex.select('notes.id', 'title', 'content', 'folder_id', 'folders.name as folder_name')
+    .from('notes')
+    .leftJoin('folders', 'notes.folder_id', 'folders.id')
+    .where(function () {
+      if (searchTerm) {
+        this.where('title', 'like', `%${searchTerm}%`);
+      }
+    })
+    .orderBy('notes.id')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+router.get('/notes/:id', (req, res, next) => {
+  const noteId = req.params.id;
+
+  knex.first('notes.id', 'notes.title', 'notes.content', 'notes.created', 'folder_id', 'folders.name as folder_name').from('notes')
+    .where('id', noteId)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
+});
+
+module.exports = router;
