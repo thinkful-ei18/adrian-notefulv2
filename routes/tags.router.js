5cc84bdb98ea05e46b7bbea98d07779de395c0e7
Adrian Ross <adriantoddross@gmail.com>
2018-02-08 12:34:33 -0500

    add router stubs

--- routes/tags.router.js
+++ routes/tags.router.js
@@ -6,7 +6,15 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
+// GET all tags
 
+//GET by tag id
+
+// Delete a tag name
+
+// Post a tag
+
+// Update tag name
 
 
 
