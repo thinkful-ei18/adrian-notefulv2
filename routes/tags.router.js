299e355ffa309d6241339c22b94d25be5ee0ad7f
Adrian Ross <adriantoddross@gmail.com>
2018-02-08 12:43:46 -0500

    add GET and GET BY ID methods

--- routes/tags.router.js
+++ routes/tags.router.js
@@ -7,17 +7,36 @@ const router = express.Router();
 const knex = require('../knex');
 
 // GET all tags
+router.get('/tags', (req, res, next) => {
+  knex.select('id', 'name')
+    .from('tags')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(next);
+});
 
 //GET by tag id
+router.get('/tags/:id', (req, res, next) => {
+  const tagsID = req.params.id;
+
+  knex.first('id', 'name').from('tags')
+    .where('id', tagsID)
+    .then(tag => {
+      if (tag) {
+        res.json(tag);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
+});
 
 // Delete a tag name
 
 // Post a tag
 
 // Update tag name
-
-
-
 
 
 
