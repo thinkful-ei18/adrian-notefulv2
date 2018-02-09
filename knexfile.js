64a72b57592da62a9d9bafeceb6cf23954129e58
Adrian <adriantoddross@gmail.com>
2018-02-09 18:03:07 -0500

    add test environment

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,11 @@ module.exports = {
     debug: false, // http://knexjs.org/#Installation-debug
     pool: {min : 1 , max : 2}
   },
+  test: {
+    client: 'pg',
+    connection: process.env.TEST_DATABASE_URL || 'postgres://localhost/noteful-test',
+    pool: {min : 1 , max : 2}
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
