6f1271c04ca046d158b8b7960fac4b75f704bd6e
Adrian <adriantoddross@gmail.com>
2018-02-08 23:48:50 -0500

    add folder_id and tags editForm.find to handleNoteFormSubmit

--- public/scripts/noteful.js
+++ public/scripts/noteful.js
@@ -147,6 +147,8 @@ const noteful = (function () {
         id: store.currentNote.id,
         title: editForm.find('.js-note-title-entry').val(),
         content: editForm.find('.js-note-content-entry').val(),
+        folder_id: editForm.find('.js-note-folder-entry').val(),
+        tags: editForm.find('.js-note-tags-entry').val()
       };
 
       if (store.currentNote.id) {
